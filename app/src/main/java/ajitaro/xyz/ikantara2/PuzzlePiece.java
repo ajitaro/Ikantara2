5b6b4977ef773f4d1c697053341e77a565edf8bd
Fitriaji Taqiy Robbaanii <ftaqiy@gmail.com>
2018-12-12 10:37:26 +0700

    add drag and drop function

--- app/src/main/java/ajitaro/xyz/ikantara2/PuzzlePiece.java
+++ app/src/main/java/ajitaro/xyz/ikantara2/PuzzlePiece.java
@@ -3,8 +3,8 @@ package ajitaro.xyz.ikantara2;
 import android.content.Context;
 
 public class PuzzlePiece extends android.support.v7.widget.AppCompatImageView {
-    public int xCord;
-    public int yCord;
+    public int xCoord;
+    public int yCoord;
     public int pieceWidth;
     public int pieceHeight;
     public boolean canMove = true;
